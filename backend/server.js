546b58bd81c1befb90d42209db5a2746f17850b3
Aditya Verma <adityahosir@gmail.com>
2026-08-31 19:57:53 +0530

    Connected DB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,10 @@
+const dotenv=require('dotenv');
 const app=require("./src/app");
+const connectDB = require("./src/db/db");
+
+dotenv.config();
+
+connectDB();
 
 app.listen(3000,()=>{
     console.log("Your server is Ruuning in Port 3000")
